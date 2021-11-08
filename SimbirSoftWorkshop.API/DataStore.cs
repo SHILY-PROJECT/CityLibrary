8a18620a1628a2c0b4bb6a239c2ccc15dca61bff
Ilya Shimbuev <iliyarock@gmail.com>
2021-11-09 00:04:05 +0300

    -Созданы классы "Human" и "Book" -Создан класс "DataStore" со списками для классов выше

---
+++ SimbirSoftWorkshop.API/DataStore.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using SimbirSoftWorkshop.API.Models;
+
+namespace SimbirSoftWorkshop.API
+{
+    internal class DataStore
+    {
+        /// <summary>
+        /// 1.2.3 - Список людей
+        /// </summary>
+        internal static List<HumanDto> Humans = new()
+        {
+            new()
+            {
+                Name = "Alexander",
+                Surname = "Statskov",
+                Patronymic = "Dmitrievich",
+                Birthday = new(1983, 03, 12)
+            },
+            new()
+            {
+                Name = "Igor",
+                Surname = "Izmailov",
+                Patronymic = "Innokentievich",
+                Birthday = new(1978, 11, 27)
+            },
+            new()
+            {
+                Name = "Yana",
+                Surname = "Sharkelova",
+                Patronymic = "Egorovna",
+                Birthday = new(1992, 05, 06)
+            }
+        };
+
+        /// <summary>
+        /// 1.2.3 - Список книг
+        /// </summary>
+        internal static List<BookDto> Books = new()
+        {
+            new()
+            {
+                Title = "Harry Potter and the Methods of Rationality",
+                Author = "Eliezer Yudkowsky",
+                Genre = "Fantasy"
+            },
+            new()
+            {
+                Title = "The Demon-Haunted World: Science as a Candle in the Dark",
+                Author = "Carl Sagan",
+                Genre = "Scientific literature"
+            },
+            new()
+            {
+                Title = "The Brain's Way of Healing: Remarkable Discoveries and Recoveries from the Frontiers of Neuroplasticity",
+                Author = "Norman Doidge",
+                Genre = "Self-development literature "
+            }
+        };
+
+    }
+}
---
+++ SimbirSoftWorkshop.API/Models/HumanDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimbirSoftWorkshop.API.Models
+{
+    /// <summary>
+    /// 1.2.1 - Класс человека
+    /// </summary>
+    public class HumanDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
