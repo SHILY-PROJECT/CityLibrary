851aaeea4f75f210d840c7508f3e49222a056b54
Ilya Shimbuev <iliyarock@gmail.com>
2022-03-07 00:17:37 +0300

    -Переработан 'PersonController'

---
+++ src/WebApi/Controllers/PersonController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Domain.Models;
+using Domain.Interfaces.Services;
+using WebApi.Models;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("/api/person")]
+public class PersonController : ControllerBase
+{
+    private readonly IPersonService _service;
+    private readonly IMapper _mapper;
+
+    public PersonController(IPersonService service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+
+    [HttpGet("{personId}")]
+    public ActionResult<PersonDto> GetPerson([FromRoute] Guid personId)
+    {
+        var person = _service.Get(personId);
+        return _mapper.Map<PersonDto>(person);
+    }
+
+    [HttpGet]
+    public ActionResult<IReadOnlyCollection<PersonDto>> GetPersons()
+    {
+        var persons = _service.GetAll();
+        var personsDto = _mapper.Map<IReadOnlyCollection<PersonDto>>(persons);
+        return Ok(personsDto);
+    }
+
+    [HttpPost]
+    public ActionResult<PersonDto> AddPerson([FromQuery] PersonDto personDto)
+    {
+        var person = _mapper.Map<Person>(personDto);
+        var res = _service.New(person);
+        return _mapper.Map<PersonDto>(res);
+    }
+
+    [HttpGet("{personId}/books")]
+    public ActionResult<IReadOnlyCollection<BookDto>> GetPersonBooks([FromRoute] Guid personId)
+    {
+        var books = _service.GetPersonBooks(personId);
+        var res = _mapper.Map<IReadOnlyCollection<BookDto>>(books);
+        return Ok(res);
+    }
+
+
+    [HttpPost]
+    public ActionResult<bool> TakeBook([FromQuery] Guid personeId, [FromQuery] Guid bookId)
+    {
+        return _service.TakeBook(personeId, bookId);
+    }
+
+    [HttpPost]
+    public ActionResult<bool> ReturnBook([FromQuery] Guid personeId, [FromQuery] Guid bookId)
+    {
+        return _service.ReturnBook(personeId, bookId);
+    }
+
+    [HttpPut]
+    public ActionResult<PersonDto> UpdatePerson([FromBody] PersonDto personDto)
+    {
+        var person = _mapper.Map<Person>(personDto);
+        var res = _service.Update(personDto.Id, person);
+        return _mapper.Map<PersonDto>(res);
+    }
+
+    [HttpDelete]
+    public ActionResult<bool> DeletePerson([FromQuery] Guid personId)
+    {
+        return _service.Delete(personId);
+    }
+
+    [HttpDelete]
+    public ActionResult<bool> DeletePerson([FromQuery] PersonDto personDto)
+    {
+        var person = _mapper.Map<Person>(personDto);
+        return _service.Delete(person);
+    }
+}
---
+++ src/WebApi/Models/PersonDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApi.Models;
+
+public class PersonDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+}
