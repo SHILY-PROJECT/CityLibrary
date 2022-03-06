c78b85757b78cd5d42e60038fbec1b4029636a8f
Ilya Shimbuev <iliyarock@gmail.com>
2022-03-07 00:27:52 +0300

    -Some changes and refactoring

---
+++ src/Domain/Models/GenreStats.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models;
+
+public class GenreStats
+{
+    public Book Book { get; set; }
+    public int Quantity { get; set; }
+}
--- src/WebApi/Repositories/PersonRepository.cs
+++ src/WebApi/Repositories/PersonRepository.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using WebApi.WebApi.Interfaces;
 using WebApi.WebApi.Models.Dto;
 using WebApi.WebApi.Models.Entity;
 using WebApi.WebApi.Models.Dto.Persons;
@@ -10,200 +9,198 @@ using WebApi.WebApi.Toolkit;
 
 namespace WebApi.WebApi.Repositories
 {
-    /// <summary>
-    /// 2.6 - Репозиторий пользователя
-    /// </summary>
-    public class PersonRepository : IPersonRepository
-    {
-        private readonly DataContext _context;
 
-        /// <summary>
-        /// 3.1.1 - Конструктор для реализации DI.
-        /// </summary>
-        public PersonRepository(DataContext context)
-        {
-            _context = context;
-        }
+    //public class PersonRepository : IPersonRepository
+    //{
+    //    private readonly DataContext _context;
 
-        /// <summary>
-        /// Добавление нового пользователя.
-        /// </summary>
-        public ResultContent<Person> Add(PersonDto personCreate)
-        {
-            try
-            {
-                var person = new Person
-                {
-                    FirstName = personCreate.FirstName,
-                    LastName = personCreate.LastName,
-                    MiddleName = personCreate.MiddleName,
-                    BirthDate = personCreate.BirthDate,
-                };
+    //    /// <summary>
+    //    /// 3.1.1 - Конструктор для реализации DI.
+    //    /// </summary>
+    //    public PersonRepository(DataContext context)
+    //    {
+    //        _context = context;
+    //    }
 
-                _context.Persons.Add(person);
-                _context.SaveChanges();
+    //    /// <summary>
+    //    /// Добавление нового пользователя.
+    //    /// </summary>
+    //    public ResultContent<Person> Add(PersonDto personCreate)
+    //    {
+    //        try
+    //        {
+    //            var person = new Person
+    //            {
+    //                FirstName = personCreate.FirstName,
+    //                LastName = personCreate.LastName,
+    //                MiddleName = personCreate.MiddleName,
+    //                BirthDate = personCreate.BirthDate,
+    //            };
 
-                return new ResultContent<Person>().Ok(person);
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<Person>().Error(ex);
-            }
-        }
+    //            _context.Persons.Add(person);
+    //            _context.SaveChanges();
 
-        /// <summary>
-        /// Обновить информацию пользователя.
-        /// </summary>
-        public ResultContent<Person> Update(UpdatePersonDto updatePerson)
-        {
-            try
-            {
-                var person = _context.Persons.Find(updatePerson.Id);
+    //            return new ResultContent<Person>().Ok(person);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<Person>().Error(ex);
+    //        }
+    //    }
 
-                if (person is null)
-                    return new ResultContent<Person>().Error($"'{nameof(updatePerson.Id)}:{updatePerson.Id}' - Пользователь не найден");
+    //    /// <summary>
+    //    /// Обновить информацию пользователя.
+    //    /// </summary>
+    //    public ResultContent<Person> Update(UpdatePersonDto updatePerson)
+    //    {
+    //        try
+    //        {
+    //            var person = _context.Persons.Find(updatePerson.Id);
 
-                person.FirstName = updatePerson.FirstName;
-                person.LastName = updatePerson.LastName;
-                person.MiddleName = updatePerson.MiddleName;
-                person.BirthDate = updatePerson.BirthDate;
+    //            if (person is null)
+    //                return new ResultContent<Person>().Error($"'{nameof(updatePerson.Id)}:{updatePerson.Id}' - Пользователь не найден");
 
-                _context.SaveChanges();
+    //            person.FirstName = updatePerson.FirstName;
+    //            person.LastName = updatePerson.LastName;
+    //            person.MiddleName = updatePerson.MiddleName;
+    //            person.BirthDate = updatePerson.BirthDate;
 
-                return new ResultContent<Person>().Ok(person);
-            }
-            catch(Exception ex)
-            {
-                return new ResultContent<Person>().Error(ex);
-            }
-        }
+    //            _context.SaveChanges();
 
-        /// <summary>
-        /// Удаление пользователя.
-        /// </summary>
-        public ResultContent<Person> Delete(int personId)
-        {
-            try
-            {
-                var person = _context.Persons.Find(personId);
+    //            return new ResultContent<Person>().Ok(person);
+    //        }
+    //        catch(Exception ex)
+    //        {
+    //            return new ResultContent<Person>().Error(ex);
+    //        }
+    //    }
 
-                if (person is null)
-                    return new ResultContent<Person>().Error($"'{nameof(personId)}:{personId}' - Пользователь не найден");
+    //    /// <summary>
+    //    /// Удаление пользователя.
+    //    /// </summary>
+    //    public ResultContent<Person> Delete(int personId)
+    //    {
+    //        try
+    //        {
+    //            var person = _context.Persons.Find(personId);
 
-                _context.Persons.Remove(person);
-                _context.SaveChanges();
+    //            if (person is null)
+    //                return new ResultContent<Person>().Error($"'{nameof(personId)}:{personId}' - Пользователь не найден");
 
-                return new ResultContent<Person>().Ok(person, "Пользователь успешно удален");
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<Person>().Error(ex);
-            }
-        }
+    //            _context.Persons.Remove(person);
+    //            _context.SaveChanges();
 
-        /// <summary>
-        /// Удаление пользователя.
-        /// </summary>
-        public ResultContent<IEnumerable<Person>> Delete(PersonDto person)
-        {
-            var firstName = person.FirstName;
-            var lastName = person.LastName;
-            var middleName = person.MiddleName;
+    //            return new ResultContent<Person>().Ok(person, "Пользователь успешно удален");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<Person>().Error(ex);
+    //        }
+    //    }
 
-            try
-            {
-                var persons = _context.Persons
-                    .Where(x => EF.Functions.Like(x.FirstName, firstName) && EF.Functions.Like(x.LastName, lastName) &&
-                        (string.IsNullOrEmpty(middleName) || EF.Functions.Like(x.MiddleName, middleName)))
-                    .ToList();
+    //    /// <summary>
+    //    /// Удаление пользователя.
+    //    /// </summary>
+    //    public ResultContent<IEnumerable<Person>> Delete(PersonDto person)
+    //    {
+    //        var firstName = person.FirstName;
+    //        var lastName = person.LastName;
+    //        var middleName = person.MiddleName;
 
-                if (persons.Any() is false)
-                    return new ResultContent<IEnumerable<Person>>().Error(
-                        $"'{nameof(firstName)}:{firstName} | {nameof(lastName)}:{lastName} | {nameof(middleName)}:{middleName}' - Пользователь не найден");
+    //        try
+    //        {
+    //            var persons = _context.Persons
+    //                .Where(x => EF.Functions.Like(x.FirstName, firstName) && EF.Functions.Like(x.LastName, lastName) &&
+    //                    (string.IsNullOrEmpty(middleName) || EF.Functions.Like(x.MiddleName, middleName)))
+    //                .ToList();
 
-                persons.ForEach(x => _context.Remove(x));
-                _context.SaveChanges();
+    //            if (persons.Any() is false)
+    //                return new ResultContent<IEnumerable<Person>>().Error(
+    //                    $"'{nameof(firstName)}:{firstName} | {nameof(lastName)}:{lastName} | {nameof(middleName)}:{middleName}' - Пользователь не найден");
 
-                return new ResultContent<IEnumerable<Person>>().Ok(persons, "Пользователь успешно удален");
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<IEnumerable<Person>>().Error(ex);
-            }
-        }
+    //            persons.ForEach(x => _context.Remove(x));
+    //            _context.SaveChanges();
 
-        /// <summary>
-        /// Получение книг пользователя.
-        /// </summary>
-        public ResultContent<IEnumerable<Book>> GetPersonBooks(int personId)
-        {
-            try
-            {
-                var books = _context.LibraryCards
-                    .Where(lc => lc.PersonId == personId)
-                    .Include(lc => lc.Book)
-                        .ThenInclude(b => b.Author)
-                    .Include(lc => lc.Book.BooksGenres)
-                        .ThenInclude(bg => bg.Genre)
-                        .Select(x => x.Book);
+    //            return new ResultContent<IEnumerable<Person>>().Ok(persons, "Пользователь успешно удален");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<IEnumerable<Person>>().Error(ex);
+    //        }
+    //    }
 
-                return new ResultContent<IEnumerable<Book>>().Ok(books);
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<IEnumerable<Book>>().Error(ex);
-            }
-        }
+    //    /// <summary>
+    //    /// Получение книг пользователя.
+    //    /// </summary>
+    //    public ResultContent<IEnumerable<Book>> GetPersonBooks(int personId)
+    //    {
+    //        try
+    //        {
+    //            var books = _context.LibraryCards
+    //                .Where(lc => lc.PersonId == personId)
+    //                .Include(lc => lc.Book)
+    //                    .ThenInclude(b => b.Author)
+    //                .Include(lc => lc.Book.BooksGenres)
+    //                    .ThenInclude(bg => bg.Genre)
+    //                    .Select(x => x.Book);
 
-        /// <summary>
-        /// Получение книги пользователем на руки.
-        /// </summary>
-        public ResultContent<LibraryCard> TakeBook(PersonBookDto personBook)
-        {
-            try
-            {
-                var libraryCard = new LibraryCard
-                {
-                    BookId = personBook.BookId,
-                    PersonId = personBook.PersoneId
-                };
-                _context.LibraryCards.Add(libraryCard);
-                _context.SaveChanges();
+    //            return new ResultContent<IEnumerable<Book>>().Ok(books);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<IEnumerable<Book>>().Error(ex);
+    //        }
+    //    }
 
-                return new ResultContent<LibraryCard>().Ok(libraryCard, "Пользователью успешно выдана книга");
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<LibraryCard>().Error(ex);
-            }
-        }
+    //    /// <summary>
+    //    /// Получение книги пользователем на руки.
+    //    /// </summary>
+    //    public ResultContent<LibraryCard> TakeBook(PersonBookDto personBook)
+    //    {
+    //        try
+    //        {
+    //            var libraryCard = new LibraryCard
+    //            {
+    //                BookId = personBook.BookId,
+    //                PersonId = personBook.PersoneId
+    //            };
+    //            _context.LibraryCards.Add(libraryCard);
+    //            _context.SaveChanges();
 
-        /// <summary>
-        /// Возврат книги пользователем с рук.
-        /// </summary>
-        public ResultContent<LibraryCard> ReturnBook(PersonBookDto personBook)
-        {
-            var bookId = personBook.BookId;
-            var personId = personBook.PersoneId;
+    //            return new ResultContent<LibraryCard>().Ok(libraryCard, "Пользователью успешно выдана книга");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<LibraryCard>().Error(ex);
+    //        }
+    //    }
 
-            try
-            {
-                var libraryCard = _context.LibraryCards.FirstOrDefault(x => x.BookId == bookId && x.PersonId == personId);
+    //    /// <summary>
+    //    /// Возврат книги пользователем с рук.
+    //    /// </summary>
+    //    public ResultContent<LibraryCard> ReturnBook(PersonBookDto personBook)
+    //    {
+    //        var bookId = personBook.BookId;
+    //        var personId = personBook.PersoneId;
 
-                if (libraryCard is null)
-                    return new ResultContent<LibraryCard>().Error(
-                        $"'{nameof(bookId)}:{bookId} | {nameof(personId)}:{personId}' - Карточка пользователя не найдена");
+    //        try
+    //        {
+    //            var libraryCard = _context.LibraryCards.FirstOrDefault(x => x.BookId == bookId && x.PersonId == personId);
 
-                _context.LibraryCards.Remove(libraryCard);
-                _context.SaveChanges();
+    //            if (libraryCard is null)
+    //                return new ResultContent<LibraryCard>().Error(
+    //                    $"'{nameof(bookId)}:{bookId} | {nameof(personId)}:{personId}' - Карточка пользователя не найдена");
 
-                return new ResultContent<LibraryCard>().Ok(libraryCard, "Пользователь успешно вернул книгу");
-            }
-            catch (Exception ex)
-            {
-                return new ResultContent<LibraryCard>().Error(ex);
-            }
-        }
+    //            _context.LibraryCards.Remove(libraryCard);
+    //            _context.SaveChanges();
 
-    }
+    //            return new ResultContent<LibraryCard>().Ok(libraryCard, "Пользователь успешно вернул книгу");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return new ResultContent<LibraryCard>().Error(ex);
+    //        }
+    //    }
+
+    //}
 }
