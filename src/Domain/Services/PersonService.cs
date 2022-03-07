208ddc5f737bb88afe5c9474971227a8b5a1f2e1
Ilya Shimbuev <iliyarock@gmail.com>
2022-03-07 20:39:06 +0300

    -Реализован сервис человека

---
+++ src/Domain/Services/PersonService.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Models;
+using Domain.Interfaces.Services;
+using Domain.Interfaces.Repositories;
+
+namespace Domain.Services;
+
+public class PersonService : IPersonService
+{
+    private readonly IPersonRepository _personRepository;
+
+    public PersonService(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public Person? Get(Guid id)
+    {
+        return _personRepository.Get(id);
+    }
+
+    public IReadOnlyCollection<Person> GetAll()
+    {
+        return _personRepository.GetAll().ToArray();
+    }
+
+    public Person New(Person model)
+    {
+        return _personRepository.New(model);
+    }
+
+    public Person Update(Guid id, Person model)
+    {
+        return _personRepository.Update(id, model);
+    }
+
+    public bool Delete(Guid id)
+    {
+        return _personRepository.Delete(id);
+    }
+
+    public bool Delete(Person person)
+    {
+        return _personRepository.Delete(person);
+    }
+
+    public IReadOnlyCollection<Book> GetPersonBooks(Guid personId)
+    {
+        return _personRepository.GetPersonBooks(personId).ToArray();
+    }
+
+    public bool TakeBook(Guid personeId, Guid bookId)
+    {
+        return _personRepository.TakeBook(personeId, bookId);
+    }
+
+    public bool ReturnBook(Guid personeId, Guid bookId)
+    {
+        return _personRepository.ReturnBook(personeId, bookId);
+    }
+}
