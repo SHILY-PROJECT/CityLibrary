5c7c1249011fb16afba3730ed24d4964060557b5
Ilya Shimbuev <iliyarock@gmail.com>
2022-03-08 12:03:21 +0300

    -Заимплеменчен репозиторий жанра

---
+++ src/DataAccess/Repositories/GenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Domain.Interfaces.Repositories;
+using DataAccess.Entities;
+using AutoMapper;
+
+namespace DataAccess.Repositories;
+
+public class GenreRepository : BaseRepository<Genre, GenreDb>, IGenreRepository
+{
+    private readonly CityLibraryDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GenreRepository(CityLibraryDbContext context, IMapper mapper) : base(context, context.Genres, mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
