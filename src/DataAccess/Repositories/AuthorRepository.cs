835f2241b83b28fb2bb937dcfd1cc8f2d2c1851a
Ilya Shimbuev <iliyarock@gmail.com>
2022-03-08 12:42:19 +0300

    -Заимплеменчен репозиторий автора

---
+++ src/DataAccess/Repositories/AuthorRepository.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
+using DataAccess.Entities;
+
+namespace DataAccess.Repositories;
+
+public class AuthorRepository : BaseRepository<Author, AuthorDb>, IAuthorRepository
+{
+    private readonly CityLibraryDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuthorRepository(CityLibraryDbContext context, IMapper mapper) : base(context, context.Authors, mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public IEnumerable<Book> GetBooksByAuthor(Guid authorId)
+    {
+        var books = _context.Books.Where(book => book.AuthorId == authorId);
+        return _mapper.Map<IEnumerable<Book>>(books);
+    }
+}
--- src/Domain/Interfaces/Repositories/IAuthorRepository.cs
+++ src/Domain/Interfaces/Repositories/IAuthorRepository.cs
@@ -4,6 +4,5 @@ namespace Domain.Interfaces.Repositories;
 
 public interface IAuthorRepository : IRepository<Author>
 {
-    bool Add(Author author, IEnumerable<Book> books);
     IEnumerable<Book> GetBooksByAuthor(Guid authorId);
 }
