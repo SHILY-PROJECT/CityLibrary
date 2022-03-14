af60382e3a1c27170b4b0874b401a4d2b39e5f92
Ilya Shimbuev <Ilya_Shimbuev@epam.com>
2022-03-14 22:43:47 +0300

    -Контроллер жанра переработан для асинхронного использования

--- src/WebApi/Controllers/GenreController.cs
+++ src/WebApi/Controllers/GenreController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using Domain.Models;
@@ -21,26 +22,26 @@ public class GenreController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IReadOnlyCollection<GenreDto>> GetGenres()
+    public async Task<ActionResult<IReadOnlyCollection<GenreDto>>> GetGenres()
     {
-        var genres = _service.GetAll();
-        var genresDto = _mapper.Map<IReadOnlyCollection<GenreDto>>(genres);
-        return Ok(genresDto);
+        var genres = await _service.GetAllAsync();
+        var genresResult = _mapper.Map<IReadOnlyCollection<GenreDto>>(genres);
+        return Ok(genresResult);
     }
 
     [HttpGet("stats")]
-    public ActionResult<IReadOnlyCollection<GenreStatsDto>> GetStats()
+    public async Task<ActionResult<IReadOnlyCollection<GenreStatsDto>>> GetStats()
     {
-        var stats = _service.GetStats();
-        var statsDto = _mapper.Map<IReadOnlyCollection<GenreStatsDto>>(stats);
-        return Ok(statsDto);
+        var stats = await _service.GetStatsAsync();
+        var statsResult = _mapper.Map<IReadOnlyCollection<GenreStatsDto>>(stats);
+        return Ok(statsResult);
     }
 
     [HttpPost]
-    public ActionResult<GenreDto> AddGenre([FromBody] GenreDto genreDto)
+    public async Task<ActionResult<GenreDto>> AddGenre([FromBody] GenreDto genreDto)
     {
         var genre = _mapper.Map<Genre>(genreDto);
-        var res = _service.New(genre);
-        return _mapper.Map<GenreDto>(res);
+        var genreResult = await _service.NewAsync(genre);
+        return _mapper.Map<GenreDto>(genreResult);
     }
 }
