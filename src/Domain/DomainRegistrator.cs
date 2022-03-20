4d7e1cceca54da8c91f6139ba9897e35169659fa
Ilya Shimbuev <Ilya_Shimbuev@epam.com>
2022-03-20 14:09:11 +0300

    -Добавлен регистратор служб для 'Domain' (бизнес-логики)

---
+++ src/Domain/DomainRegistrator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Domain.Interfaces.Services;
+using Domain.Services;
+
+namespace Domain;
+
+public static class DomainRegistrator
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IAuthorService, AuthorService>()
+            .AddScoped<IBookService, BookService>()
+            .AddScoped<IGenreService, GenreService>()
+            .AddScoped<IPersonService, PersonService>();
+
+        return services;
+    }
+}
