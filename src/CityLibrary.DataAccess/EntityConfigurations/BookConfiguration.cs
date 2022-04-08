b2c4a2051e3f1a36427b56f65092ba6636b92bec
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-08 23:58:51 +0300

    -Добавлены настройки связей в таблицу книг

--- src/CityLibrary.DataAccess/EntityConfigurations/BookConfiguration.cs
+++ src/CityLibrary.DataAccess/EntityConfigurations/BookConfiguration.cs
@@ -4,16 +4,24 @@ using CityLibrary.DataAccess.Entities;
 
 namespace CityLibrary.DataAccess.EntityConfigurations;
 
-public class BookConfiguration : IEntityTypeConfiguration<BookDb>
+internal sealed class BookConfiguration : IEntityTypeConfiguration<BookDb>
 {
     public void Configure(EntityTypeBuilder<BookDb> builder)
     {
-        builder.HasKey(x => x.Id);
+        builder.HasKey(b => b.Id);
 
-        builder.Property(x => x.Id).ValueGeneratedOnAdd();
-        builder.Property(x => x.Name).HasMaxLength(500).IsRequired();
-        builder.Property(x => x.AuthorId).IsRequired();
+        builder.Property(b => b.Id).ValueGeneratedOnAdd();
+        builder.Property(b => b.Name).HasMaxLength(500).IsRequired();
+        builder.Property(b => b.AuthorId).IsRequired();
 
-        builder.HasOne(x => x.Author).WithMany();
+        builder
+            .HasOne(b => b.Author)
+            .WithMany()
+            .HasForeignKey(b => b.AuthorId);
+
+        builder
+            .HasOne(b => b.Genre)
+            .WithMany()
+            .HasForeignKey(b => b.GenreId);
     }
 }
