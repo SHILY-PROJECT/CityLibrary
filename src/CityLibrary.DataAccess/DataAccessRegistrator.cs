8d63a4b6a94b8f38971dee1d7b49f93934dfe983
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-09 01:47:05 +0300

    -В DataAccess добавлена регистрация настроек создания базы данных

--- src/CityLibrary.DataAccess/DataAccessRegistrator.cs
+++ src/CityLibrary.DataAccess/DataAccessRegistrator.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CityLibrary.Domain.Interfaces.Repositories;
+using CityLibrary.DataAccess.Models;
 using CityLibrary.DataAccess.Repositories;
 
 namespace CityLibrary.DataAccess;
 
 public static class DataAccessRegistrator
 {
-    public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString)
+    public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString, DatabaseCreationSettings databaseCreationSettings)
     {
         services
             .AddScoped<IAuthorRepository, AuthorRepository>()
             .AddScoped<IBookRepository, BookRepository>()
             .AddScoped<IGenreRepository, GenreRepository>()
             .AddScoped<IPersonRepository, PersonRepository>();
+
+        services.AddScoped(x => databaseCreationSettings);
 
         services
             .AddAutoMapper(typeof(DataAccess.MapperProfile))
